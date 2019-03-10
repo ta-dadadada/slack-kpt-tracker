5bf9c5f0ddd40284c19417aaabfb484ca248e60f
ta-dadadada <94or2.t@gmail.com>
2019-03-10 16:18:03 +0900

    refs #8 slackからTRYを操作できる

--- src/api/main.go
+++ src/api/main.go
@@ -12,6 +12,7 @@ import (
 
 var keepRex = regexp.MustCompile(`<.+>\s(?i)(k|keep)[\s　](.+)`)
 var problemRex = regexp.MustCompile(`<.+>\s(?i)(p|problem)[\s　](.+)`)
+var tryRex = regexp.MustCompile(`<.+>\s(?i)(t|try)[\s　](.+)`)
 
 // FIXME リファクタ。ネストやばすぎ
 func generateKeepMessage(user *Users, inText string) (outText string) {
@@ -19,32 +20,32 @@ func generateKeepMessage(user *Users, inText string) (outText string) {
 	if len(matches) > 0 && matches[0][2] != "" {
 		body := matches[0][2]
 		if body == "list" {
-			keeps, err := GetKeepList(user.UserID)
+			entities, err := GetKeepList(user.UserID)
 			if err != nil {
 				log.Fatal(err)
 				outText = "keepの取得に失敗しました"
 				return
 			}
-			if len(keeps) == 0 {
+			if len(entities) == 0 {
 				outText = "keepが登録されていません"
 				return
 			}
 			// FIXME 読みやすさ重視。GOの文字列結合コスト的にどうなのか不明
 			sb := strings.Builder{}
 			sb.WriteString("KEEP\n============\n")
-			for i, keep := range keeps {
+			for i, ent := range entities {
 				sb.WriteString(
-					fmt.Sprintf("%v. %v\n", i+1, keep.Body))
+					fmt.Sprintf("%v. %v\n", i+1, ent.Body))
 			}
 			outText = sb.String()
 			return
 		}
-		keep, err := CreateKeep(user.UserID, body)
+		ent, err := CreateKeep(user.UserID, body)
 		if err != nil {
 			outText = "Keepの記録に失敗しました"
 			return
 		}
-		outText = fmt.Sprintf("Keepを記録しました: `%v`", keep.Body)
+		outText = fmt.Sprintf("Keepを記録しました: `%v`", ent.Body)
 		return
 	}
 	outText = ""
@@ -57,32 +58,70 @@ func generateProblemMessage(user *Users, inText string) (outText string) {
 	if len(matches) > 0 && matches[0][2] != "" {
 		body := matches[0][2]
 		if body == "list" {
-			keeps, err := GetProblemList(user.UserID)
+			entities, err := GetProblemList(user.UserID)
 			if err != nil {
 				log.Fatal(err)
 				outText = "Problemの取得に失敗しました"
 				return
 			}
-			if len(keeps) == 0 {
+			if len(entities) == 0 {
 				outText = "Problemが登録されていません"
 				return
 			}
 			// FIXME 読みやすさ重視。GOの文字列結合コスト的にどうなのか不明
 			sb := strings.Builder{}
 			sb.WriteString("PROBLEM\n============\n")
-			for i, problem := range keeps {
+			for i, problem := range entities {
 				sb.WriteString(
 					fmt.Sprintf("%v. %v\n", i+1, problem.Body))
 			}
 			outText = sb.String()
 			return
 		}
-		keep, err := CreateProblem(user.UserID, body)
+		ent, err := CreateProblem(user.UserID, body)
 		if err != nil {
 			outText = "Problemの記録に失敗しました"
 			return
 		}
-		outText = fmt.Sprintf("Problemを記録しました: `%v`", keep.Body)
+		outText = fmt.Sprintf("Problemを記録しました: `%v`", ent.Body)
+		return
+	}
+	outText = ""
+	return
+}
+
+// FIXME リファクタ。ネストやばすぎ
+func generateTryMessage(user *Users, inText string) (outText string) {
+	matches := tryRex.FindAllStringSubmatch(inText, -1)
+	if len(matches) > 0 && matches[0][2] != "" {
+		body := matches[0][2]
+		if body == "list" {
+			entities, err := GetTryList(user.UserID)
+			if err != nil {
+				log.Fatal(err)
+				outText = "Tryの取得に失敗しました"
+				return
+			}
+			if len(entities) == 0 {
+				outText = "Tryが登録されていません"
+				return
+			}
+			// FIXME 読みやすさ重視。GOの文字列結合コスト的にどうなのか不明
+			sb := strings.Builder{}
+			sb.WriteString("Try\n============\n")
+			for i, ent := range entities {
+				sb.WriteString(
+					fmt.Sprintf("%v. %v\n", i+1, ent.Body))
+			}
+			outText = sb.String()
+			return
+		}
+		ent, err := CreateTry(user.UserID, body)
+		if err != nil {
+			outText = "Tryの記録に失敗しました"
+			return
+		}
+		outText = fmt.Sprintf("Tryを記録しました: `%v`", ent.Body)
 		return
 	}
 	outText = ""
@@ -95,6 +134,9 @@ func getReplyMessage(user *Users, data slack.MessageEvent) (replyMessage string)
 		return
 	}
 	if replyMessage = generateProblemMessage(user, text); replyMessage != "" {
+		return
+	}
+	if replyMessage = generateTryMessage(user, text); replyMessage != "" {
 		return
 	}
 	replyMessage = "使い方\n" + "====================\n" +
